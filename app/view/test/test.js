c46d1f5350fe5db0a77c2ab3f4dcf3a337cd4afb
kimhun55 <kimhun55@gmail.com>
2017-03-18 13:40:24 +0700

    ui login new

--- app/view/test/test.js
+++ app/view/test/test.js
@@ -3,6 +3,7 @@ var stdModule = require("~/student-view-model");
 var frameModule = require("ui/frame");
 var observableModule = require("data/observable")
 var dialogs = require("ui/dialogs");
+var appSettings = require("application-settings");
 
 let context = new observableModule.fromObject({
       user : "",
@@ -12,25 +13,31 @@ exports.onPageLoaded = (args)=>{
     console.log("Page Loaded");
     var page = args.object;
     page.bindingContext = context;
+
+    context.user = appSettings.getString("user");
+    context.pass = appSettings.getString("pass");
+
 }
 
 exports.Onlogin = ()=>{
+    appSettings.setString("user", context.user);
+    appSettings.setString("pass", context.pass);
     
      fetch("http://std-card.lbtech.ac.th/api_login.php", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ user:context.user,pass:context.pass })
             }).then(r => { return r.json(); }).then(function (r) {
-                console.dump(r);
-                 console.log(r.status)
+               // console.dump(r);
+                // console.log(r.status)
                  if(r.status){
                      console.log("ok login");
                      context.data = r.data
-                     console.dump(context.data)
+                    // console.dump(context.data)
                     var topmost = frameModule.topmost();
-                    topmost.navigate({ 
+                    topmost.navigate({
                     moduleName: "view/test2/test2", 
-                    context: context
+                    context: context,
                     });
 
                  }else{
@@ -38,17 +45,12 @@ exports.Onlogin = ()=>{
                  }
 
             }).catch((error)=>{
+                console.log('erro catch')
                 console.dump(error);
             });
 
 
 }
-
-
-
-
-
-
 
 exports.goto2 = ()=>{
     var topmost = frameModule.topmost();
@@ -59,3 +61,10 @@ exports.goto2 = ()=>{
 };
 topmost.navigate(navigationEntry);
 }
+
+let setting ={
+    h:'',
+    b:''
+}
+/*JSON.stringify => json to str*/
+/*JSON.pares() => str to json*/
--- app/view/test2/test2.js
+++ app/view/test2/test2.js
@@ -3,54 +3,63 @@ var context
 var camera = require("nativescript-camera");
 
 var bghttp = require("nativescript-background-http");
- 
 var session = bghttp.session("image-upload");
 
+var page
 exports.onPageLoaded = (args) => {
 
     camera.requestPermissions();
-    var page = args.object;
+    page = args.object;
     context = page.navigationContext;
     console.log("ok test 2")
-    console.dump(context)
-    console.dump(page.navigationContext)
+    //console.dump(context)
+    //console.dump(page.navigationContext)
     page.bindingContext = context.data
 }
 
 
 exports.Onpic = ()=>{
 
-    var options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true };
+    var options = { width: 300, height: 300, keepAspectRatio: true, saveToGallery: true };
 camera.takePicture(options)   
     .then(function (imageAsset) {
 
         var showpic = page.getViewById('pic');
         showpic.src = imageAsset;
-        /*
+
+         console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+        console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+        console.log("Photo saved in Photos/Gallery for Android or in Camera Roll for iOS");
+        
         var request = {
-                url: "http://192.168.8.171:3000/upload",
+                url: "http://std-card.lbtech.ac.th/api_upload.php",
                 //url:"http://webbass.bclbtech.com/long.php",
+                //url : "http://192.168.8.109:3000/upload",
                 method: "POST",
                 // timeout: 5,
                 headers: {
                     "Content-Type": "application/octet-stream",
                     "File-Name": "bigpig.jpg",
                     //"x-code": moment().format("D_M_YYYY_H_s")
-                    "x-code": "testtest33"
+                    "x-code": context.data.student_id+".jpg"
                 },
                 description: "{ 'uploading': 'bigpig.jpg' }"
             };
+            console.dump(request)
             
             var task = session.uploadFile(imageAsset.android || imageAsset.ios, request);
             
             task.on("progress", logEvent);
             task.on("error", logEvent);
-            task.on("complete", logEvent);*/
+            task.on("complete", logEvent);
 
-        console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
-        console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
-        console.log("Photo saved in Photos/Gallery for Android or in Camera Roll for iOS");
+       
     }).catch(function (err) {
         console.log("Error -> " + err.message);
     });
+}
+
+function logEvent(e) {
+    console.log(e.eventName);
+  //  alert(e.eventName);
 }
