5820ac854d0261e44495484c544db3d940749e66
kimhun55 <kimhun55@gmail.com>
2017-03-18 09:28:22 +0700

    add show pic (new npm install)

--- app/view/test2/test2.js
+++ app/view/test2/test2.js
@@ -1,6 +1,11 @@
 
 var context
 var camera = require("nativescript-camera");
+
+var bghttp = require("nativescript-background-http");
+ 
+var session = bghttp.session("image-upload");
+
 exports.onPageLoaded = (args) => {
 
     camera.requestPermissions();
@@ -18,6 +23,30 @@ exports.Onpic = ()=>{
     var options = { width: 300, height: 300, keepAspectRatio: false, saveToGallery: true };
 camera.takePicture(options)   
     .then(function (imageAsset) {
+
+        var showpic = page.getViewById('pic');
+        showpic.src = imageAsset;
+        /*
+        var request = {
+                url: "http://192.168.8.171:3000/upload",
+                //url:"http://webbass.bclbtech.com/long.php",
+                method: "POST",
+                // timeout: 5,
+                headers: {
+                    "Content-Type": "application/octet-stream",
+                    "File-Name": "bigpig.jpg",
+                    //"x-code": moment().format("D_M_YYYY_H_s")
+                    "x-code": "testtest33"
+                },
+                description: "{ 'uploading': 'bigpig.jpg' }"
+            };
+            
+            var task = session.uploadFile(imageAsset.android || imageAsset.ios, request);
+            
+            task.on("progress", logEvent);
+            task.on("error", logEvent);
+            task.on("complete", logEvent);*/
+
         console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
         console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
         console.log("Photo saved in Photos/Gallery for Android or in Camera Roll for iOS");
